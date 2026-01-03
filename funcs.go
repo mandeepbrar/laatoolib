32e87289b0fad2be9aa664abf4ab58cac3cb67ee
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-01-03 12:53:26 +0530

    chagnes to function

--- funcs.go
+++ funcs.go
@@ -1,19 +1,37 @@
 package lib
 
 import (
+	"fmt"
+
 	"dario.cat/mergo"
 	"laatoo.io/sdk/utils"
 )
 
-func MergeMaps(obj1, obj2 utils.StringMap) utils.StringMap {
+func MergeMaps(obj1, obj2 utils.StringMap) (utils.StringMap, error) {
 	if obj1 == nil {
-		return obj2
+		return obj2, nil
 	}
 	if obj2 == nil {
-		return obj1
+		return obj1, nil
 	}
-	res := make(map[string]interface{})
-	mergo.Merge(&res, map[string]interface{}(obj1))
-	mergo.Merge(&res, map[string]interface{}(obj2))
-	return utils.StringMap(res)
+	res := make(map[string]interface{}, len(obj1))
+	for k, v := range obj1 {
+		res[k] = v
+	}
+	options := []func(*mergo.Config){mergo.WithOverride}
+
+	mergo.Merge(&res, obj2, options...)
+	return utils.StringMap(res), nil
+}
+
+func ConvertToStringsMap(val map[string]interface{}) utils.StringsMap {
+	if val == nil {
+		return nil
+	}
+	strMp := make(utils.StringsMap)
+	for k, v := range val {
+		strV := fmt.Sprintf("%v", v)
+		strMp[k] = strV
+	}
+	return strMp
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,12 @@
 module laatoo.io/lib
 
-go 1.24.5
+go 1.25.5
 
 require (
 	dario.cat/mergo v1.0.2
 	github.com/peterbourgon/diskv v2.0.1+incompatible
 	github.com/pmylund/go-cache v2.1.0+incompatible
-	laatoo.io/sdk v0.1.14
+	laatoo.io/sdk v0.1.56
 )
 
 require github.com/google/btree v1.1.3 // indirect
