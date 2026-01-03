f165ac54e3be4ab54f54d17e21b070c39df1a8c0
Mandeep Singh Brar <mandeep@virsainfomedia.com>
2026-01-03 13:28:29 +0530

    removed mergo

--- funcs.go
+++ funcs.go
@@ -3,25 +3,119 @@ package lib
 import (
 	"fmt"
 
-	"dario.cat/mergo"
 	"laatoo.io/sdk/utils"
 )
 
-func MergeMaps(obj1, obj2 utils.StringMap) (utils.StringMap, error) {
-	if obj1 == nil {
-		return obj2, nil
+// Merge performs a shallow merge of two maps (no deep nesting)
+// Works with any map type: map[string]string, map[string]int, etc.
+func Merge[K comparable, V any](m1, m2 map[K]V) map[K]V {
+	if m1 == nil {
+		return copyMap(m2)
 	}
-	if obj2 == nil {
-		return obj1, nil
+	if m2 == nil {
+		return copyMap(m1)
 	}
-	res := make(utils.StringMap, len(obj1))
-	for k, v := range obj1 {
-		res[k] = v
-	}
-	options := []func(*mergo.Config){mergo.WithOverride}
 
-	err := mergo.Merge(&res, obj2, options...)
-	return utils.StringMap(res), err
+	result := make(map[K]V, len(m1)+len(m2))
+
+	// Copy m1
+	for k, v := range m1 {
+		result[k] = v
+	}
+
+	// Override with m2
+	for k, v := range m2 {
+		result[k] = v
+	}
+
+	return result
+}
+
+// copyMap creates a shallow copy of a map
+func copyMap[K comparable, V any](m map[K]V) map[K]V {
+	if m == nil {
+		return nil
+	}
+	result := make(map[K]V, len(m))
+	for k, v := range m {
+		result[k] = v
+	}
+	return result
+}
+
+// DeepMerge performs a deep merge for maps with interface{} values
+// Recursively merges nested maps
+func DeepMerge[K comparable](m1, m2 map[K]interface{}) map[K]interface{} {
+	if m1 == nil {
+		return deepCopy(m2)
+	}
+	if m2 == nil {
+		return deepCopy(m1)
+	}
+
+	result := make(map[K]interface{}, len(m1))
+
+	// Copy everything from m1 first
+	for k, v := range m1 {
+		result[k] = deepCopyValue(v)
+	}
+
+	// Merge from m2 (with override)
+	for k, v := range m2 {
+		if existing, exists := result[k]; exists {
+			// If both are maps, merge them recursively
+			if existingMap, ok1 := existing.(map[K]interface{}); ok1 {
+				if vMap, ok2 := v.(map[K]interface{}); ok2 {
+					result[k] = DeepMerge(existingMap, vMap)
+					continue
+				}
+			}
+			// Also handle generic map[string]interface{} case
+			if existingMap, ok1 := existing.(map[string]interface{}); ok1 {
+				if vMap, ok2 := v.(map[string]interface{}); ok2 {
+					result[k] = DeepMerge(existingMap, vMap)
+					continue
+				}
+			}
+		}
+		// Override or add new value
+		result[k] = deepCopyValue(v)
+	}
+
+	return result
+}
+
+func deepCopy[K comparable](m map[K]interface{}) map[K]interface{} {
+	if m == nil {
+		return nil
+	}
+	result := make(map[K]interface{}, len(m))
+	for k, v := range m {
+		result[k] = deepCopyValue(v)
+	}
+	return result
+}
+
+func deepCopyValue(v interface{}) interface{} {
+	switch val := v.(type) {
+	case map[string]interface{}:
+		return deepCopy(val)
+	case map[interface{}]interface{}:
+		result := make(map[interface{}]interface{}, len(val))
+		for k, v := range val {
+			result[k] = deepCopyValue(v)
+		}
+		return result
+	case []interface{}:
+		result := make([]interface{}, len(val))
+		for i, item := range val {
+			result[i] = deepCopyValue(item)
+		}
+		return result
+	default:
+		// For primitive types, direct assignment is fine
+		return v
+	}
 }
 
 func ConvertToStringsMap(val map[string]interface{}) utils.StringsMap {
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module laatoo.io/lib
 go 1.25.5
 
 require (
-	dario.cat/mergo v1.0.2
 	github.com/peterbourgon/diskv v2.0.1+incompatible
 	github.com/pmylund/go-cache v2.1.0+incompatible
 	laatoo.io/sdk v0.1.56
